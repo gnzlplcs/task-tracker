2c9357a843f9f3fe54a8e5c62dfbd83f2a890905
gnzlplcs <palaciosgm@outlook.com>
2022-03-13 16:55:19 -0500

    :construction: delete task function implemented

--- src/App.js
+++ src/App.js
@@ -24,10 +24,18 @@ function App() {
     }
   ]);
 
+  // delete function
+  const deleteTask = id => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      <Tasks
+        tasks={tasks}
+        onDelete={deleteTask}
+      />
     </div>
   );
 }
