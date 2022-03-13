808d6205f1d8a2544f6db99285ff26dca1249f26
gnzlplcs <palaciosgm@outlook.com>
2022-03-13 11:38:52 -0500

    :sparkles: add useState rather than hard code

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,28 +1,32 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'Sacar a Wanda',
-    day: 'Mar 15th at 9.00pm',
-    reminder: true
-  },
-  {
-    id: 2,
-    text: 'Lavar los platos',
-    day: 'Mar 15th at 9.00pm',
-    reminder: true
-  },
-  {
-    id: 3,
-    text: 'Comprar alimentos',
-    day: 'Mar 15th at 9.00pm',
-    reminder: true
-  }
-]
+import { useState } from "react";
 
 const Tasks = () => {
+  const [ tasks, setTasks ] = useState([
+    {
+      id: 1,
+      text: 'Sacar a Wanda',
+      day: 'Mar 15th at 9.00pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Lavar los platos',
+      day: 'Mar 15th at 9.00pm',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Comprar alimentos',
+      day: 'Mar 15th at 9.00pm',
+      reminder: true
+    }
+  ]);
+
   return (
     <>
-      {tasks.map((task) => (<h3 key={task.id  }>{task.text}</h3>))}
+      {tasks.map((task) => (
+        <h3 key={task.id  }>{task.text}</h3>
+      ))}
     </>
   )
 }
