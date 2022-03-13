aeb7020b7d88ed9efbcfe2917066683540e7e7ce
gnzlplcs <palaciosgm@outlook.com>
2022-03-12 22:12:19 -0500

    🧱  add Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import Button from './Button';
 
 const Header = ({ title }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
+      <Button color='green' text='Add'/>
     </header>
   )
 }
